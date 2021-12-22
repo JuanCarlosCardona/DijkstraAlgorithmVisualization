3180254fe3c89073713540c6926f1f31835c4d69
JuanCarlosCardona <juan.cardona@train.ia.center>
2021-12-22 15:05:50 -0700

    Modifications to Algorithm, Node and Spot class implemented.

--- core/src/com/dijkstra/game/Algorithm.java
+++ core/src/com/dijkstra/game/Algorithm.java
@@ -1,6 +1,56 @@
 package com.dijkstra.game;
 
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.*;
+
 public class Algorithm
 {
+    private final int V = Dijkstra.ROWS * Dijkstra.WIDTH;
+    private final int[] dist = new int[V];
+    private final Set<Spot> visited = new HashSet<>();
+    private final PriorityQueue<Node> nodePriorityQueue = new PriorityQueue<>();
+    private final List<List<Node>> adjacencyList = new LinkedList<>();
+
+
+    public void dijkstraAlgorithm(Spot start, Spot end)
+    {
+        nodePriorityQueue.add(new Node(start, 0, null));
+        dist[0] = 0;
+        visited.add(start);
+        int count = 0;
+
+        while(!nodePriorityQueue.isEmpty())
+        {
+            Node current = nodePriorityQueue.poll();
+            visited.remove(current.item);
+
+            if(current.item.equals(end))
+                return;
+
+            for(Node neighbor : current.item.getNeighbors())
+            {
+                int temp_dist = count + 1;
+
+                if(temp_dist < neighbor.cost)
+                {
+                    neighbor.cameFrom = current;
+                    neighbor.cost = temp_dist;
+
+                    if(!visited.contains(neighbor.item))
+                    {
+                        count++;
+                        nodePriorityQueue.add(neighbor);
+                        neighbor.item.setColor(Color.GREEN);
+                    }
+                }
+
+            }
+
+            if(!current.item.equals(start))
+                current.item.setColor(Color.ROYAL);
+        }
+
+    }
 
 }
--- core/src/com/dijkstra/game/Node.java
+++ core/src/com/dijkstra/game/Node.java
@@ -1,2 +1,38 @@
-package com.dijkstra.game;public class Node {
+package com.dijkstra.game;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Node implements Comparable<Node>
+{
+    public Spot item;
+    public int cost = Integer.MAX_VALUE;
+    public Node cameFrom;
+
+    public Node(){
+        this.item = null;
+        this.cameFrom = null;
+    }
+
+    public Node(Spot item, int cost)
+    {
+        this.item = item;
+        this.cost = cost;
+    }
+
+    public Node(Spot item, int cost, Node cameFrom)
+    {
+        this.item = item;
+        this.cost = cost;
+        this.cameFrom = cameFrom;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.cost - o.cost;
+    }
+
+
 }
